d5bda9846a76265b0773be3eb5f24ee9b8cbd164
Constdesire <constantinobarbosa@gmail.com>
2026-03-11 23:00:19 -0300

    mongo

--- spring/src/main/java/io/github/fatec/repository/mongo/PessoaRepositoryWithMongoDB.java
+++ spring/src/main/java/io/github/fatec/repository/mongo/PessoaRepositoryWithMongoDB.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepositoryWithMongoDB extends MongoRepository<ClienteOrmMongo, String> {
+public interface PessoaRepositoryWithMongoDB extends MongoRepository<ClienteOrmMongo, String> {
 }
--- spring/src/main/java/io/github/fatec/repository/orm/PessoaOrmMongo.java
+++ spring/src/main/java/io/github/fatec/repository/orm/PessoaOrmMongo.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(value = "cliente")
-public record ClienteOrmMongo(
+public record PessoaOrmMongo(
         @Id
         String id,
         String nome,
